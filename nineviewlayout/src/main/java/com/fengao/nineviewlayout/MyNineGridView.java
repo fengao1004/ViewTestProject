b016a0d982b93f918ff5318b0a9ab48ad92101a8
fengao <fengaoo@ronglian.com>
2018-08-07 20:21:37 +0800

    修改测试

---
+++ nineviewlayout/src/main/java/com/fengao/nineviewlayout/MyNineGridView.java
@@ -0,0 +1,22 @@
+package com.fengao.nineviewlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class MyNineGridView extends ViewGroup {
+    public MyNineGridView(Context context) {
+        this(context, null);
+    }
+
+    public MyNineGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+}
